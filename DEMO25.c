bcf4f640de749c7dcf01839b5e99079d8cf0f14d
WilltyX <94588069+WilltyX@users.noreply.github.com>
2022-02-14 23:09:31 +0800

    Add files via upload

---
+++ DEMO25.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >= 20) break;
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b - 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int k = 5;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int left = 0;
+	int right = sz - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到！\n");
+	}
+
+	return 0;
+}
