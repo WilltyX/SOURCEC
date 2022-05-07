012a3c482ec038b6cea97e0236a4abcc6462348f
WilltyX <94588069+WilltyX@users.noreply.github.com>
2022-05-07 23:48:30 +0800

    Add files via upload

---
+++ 1.4.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a + b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int t, a, b;
+//	scanf("%d", &t);
+//	while (t--)
+//	{
+//		scanf("%d %d", &a, &b);
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) != EOF)
+//	{
+//		if (a + b == 0)
+//			break;
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
