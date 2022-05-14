9b82c33ab96de8d8d4b6b2090e3511702a51c694
WilltyX <94588069+WilltyX@users.noreply.github.com>
2022-05-14 23:28:41 +0800

    Add files via upload

---
+++ 1.9.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>//预处理指令 包含头文件 std io
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a + b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, t;
+//	scanf("%d", &t);
+//	while (t--)
+//	{
+//		scanf("%d %d", &a, &b);
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while ((scanf("%d %d", &a, &b) != EOF))
+//	{
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) != EOF)
+//	{
+//		if ((a == 0) && (b == 0))
+//			break;
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) && (a || b))
+//	{
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) && (a || b))
+//	{
+//		printf("%d", a + b);
+//	}
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
